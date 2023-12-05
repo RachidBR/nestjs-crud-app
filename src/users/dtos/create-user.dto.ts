60a638dbd0bcdb7141d6496868c088600af6c60c
RachidBR <rachid.bouadi68@gmail.com>
2023-12-05 11:10:10 +0100

    added swagger docs

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,9 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  /**
+   * A list of user's roles
+   * @example ['admin']
+   */
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsString()
+  @ApiProperty()
   password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -14,18 +14,28 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@Controller('auth')
+@ApiTags('users')
+@Controller('users')
+@ApiBearerAuth()
 @Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
-    this.usersService.create(body.email, body.password);
+  @Get()
+  @ApiOperation({
+    summary: 'Find all users',
+    description: 'Returns the list of all users or by using queries',
+  })
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
-
   @Get('/:id')
+  @ApiOperation({
+    summary: 'Find one user',
+    description: "Finds a user by id",
+  })
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(+id);
     if (!user) {
@@ -34,18 +44,29 @@ export class UsersController {
     return user;
   }
 
-  @Get()
-  findAllUsers(@Query('email') email: string) {
-    return this.usersService.find(email);
-  }
-
-  @Delete('/:id')
-  removeUser(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Post('/signup')
+  @ApiOperation({
+    summary: 'Sign up a new user',
+    description: 'Signs up a new user by giving a new email and password',
+  })
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
   }
 
   @Patch('/:id')
+  @ApiOperation({
+    summary: 'Update an existing user',
+    description: "Updates an existing user's information by id",
+  })
   UpdateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.usersService.update(+id, body);
+  }
+  @Delete('/:id')
+  @ApiOperation({
+    summary: 'Delete an existing user',
+    description: 'Deletes an existing user by id',
+  })
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(+id);
   }
 }
